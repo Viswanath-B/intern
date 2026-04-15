83f9301dbeeeada414cc75f303b00689d74201a1
BATTHALA VISWANATH <155953977+Viswanath-B@users.noreply.github.com>
2026-04-15 17:27:14 +0530

    Update applicationRoutes.js

--- server/routes/applicationRoutes.js
+++ server/routes/applicationRoutes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { createApplication } from "../controllers/applicationController.js";
+import { createApplication, testEmail } from "../controllers/applicationController.js";
 import { receiptUpload } from "../middleware/upload.js";
 
 const applicationRouter = Router();
 
 applicationRouter.post("/apply", receiptUpload.single("paymentScreenshot"), createApplication);
+applicationRouter.get("/test-email", testEmail);
 
 export default applicationRouter;
